4257b6756722d6e8adfb1d03cb7bcfb85b04c3af
Ann Lukashenko <57502653+anka1010@users.noreply.github.com>
2025-01-15 11:32:22 +0300

    Add /base parameter. Remove /dist from version control

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://anka1010.github.io",
-  // base: "/10-yellow-kitchen",
+  base: "/10-yellow-kitchen",
 });
